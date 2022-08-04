a98f5ca42156d7207d32c0ec161da166bcf6870c
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-08-04 13:22:11 +0530

    Added tests for BasicAuthentication - validateCredentials()

---
+++ core/src/test/scala/com/clairvoyant/restonomer/core/authentication/BasicAuthenticationSpec.scala
@@ -0,0 +1,51 @@
+package com.clairvoyant.restonomer.core.authentication
+
+import com.clairvoyant.restonomer.core.CoreSpec
+import com.clairvoyant.restonomer.core.exceptions.RestonomerContextException
+import com.clairvoyant.restonomer.core.model.CredentialConfig
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+class BasicAuthenticationSpec extends CoreSpec {
+
+  "validateCredentials - with empty credentials" should "throw RestonomerContextException" in {
+    val credentialConfig = CredentialConfig()
+    val basicAuthentication = new BasicAuthentication(credentialConfig)
+
+    the[RestonomerContextException] thrownBy basicAuthentication.validateCredentials() should have message
+      "The provided credentials are invalid. The credentials should contain either basicToken or both user-name & password."
+  }
+
+  "validateCredentials - with only user-name" should "throw RestonomerContextException" in {
+    val credentialConfig = CredentialConfig(userName = Some("test_user"))
+    val basicAuthentication = new BasicAuthentication(credentialConfig)
+
+    the[RestonomerContextException] thrownBy basicAuthentication.validateCredentials() should have message
+      "The provided credentials are invalid. The credentials should contain the password."
+  }
+
+  "validateCredentials - with only password" should "throw RestonomerContextException" in {
+    val credentialConfig = CredentialConfig(password = Some("test_password"))
+    val basicAuthentication = new BasicAuthentication(credentialConfig)
+
+    the[RestonomerContextException] thrownBy basicAuthentication.validateCredentials() should have message
+      "The provided credentials are invalid. The credentials should contain the user-name."
+  }
+
+  "validateCredentials - with only basic-token" should "not throw RestonomerContextException" in {
+    val credentialConfig = CredentialConfig(basicToken = Some("test_token"))
+    val basicAuthentication = new BasicAuthentication(credentialConfig)
+
+    noException should be thrownBy basicAuthentication.validateCredentials()
+  }
+
+  "validateCredentials - with only user-name and password" should "not throw RestonomerContextException" in {
+    val credentialConfig = CredentialConfig(
+      userName = Some("test_user"),
+      password = Some("test_password")
+    )
+    val basicAuthentication = new BasicAuthentication(credentialConfig)
+
+    noException should be thrownBy basicAuthentication.validateCredentials()
+  }
+
+}
