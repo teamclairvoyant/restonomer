673d3bd8fbb1a504b8b8a11d1969f453046981ef
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-08-04 18:58:02 +0530

    Added tests for RestonomerRequest - authenticate()

---
+++ core/src/test/scala/com/clairvoyant/restonomer/core/http/RestonomerRequestSpec.scala
@@ -0,0 +1,45 @@
+package com.clairvoyant.restonomer.core.http
+
+import com.clairvoyant.restonomer.core.CoreSpec
+import com.clairvoyant.restonomer.core.model.{AuthenticationConfig, CredentialConfig, RequestConfig}
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import sttp.client3.Request
+
+class RestonomerRequestSpec extends CoreSpec {
+
+  "apply" should "return RestonomerRequest object" in {
+    val requestConfig = RequestConfig(method = Some("GET"), url = "https://test-domain/url")
+    RestonomerRequest(requestConfig) shouldBe a[RestonomerRequest]
+    RestonomerRequest(requestConfig).httpRequest shouldBe a[Request[Either[String, String], Any]]
+  }
+
+  "authenticate - without authenticationConfig" should "return RestonomerRequest object with the same httpRequest" in {
+    val requestConfig = RequestConfig(method = Some("GET"), url = "https://test-domain/url")
+    val authenticationConfig = None
+
+    val restonomerRequest = RestonomerRequest(requestConfig)
+    val httpRequest = restonomerRequest.httpRequest
+
+    restonomerRequest.authenticate(authenticationConfig) shouldBe a[RestonomerRequest]
+    restonomerRequest.authenticate(authenticationConfig).httpRequest should be theSameInstanceAs httpRequest
+  }
+
+  "authenticate - with authenticationConfig" should "return RestonomerRequest object with the new authenticated httpRequest" in {
+    val requestConfig = RequestConfig(method = Some("GET"), url = "https://test-domain/url")
+
+    val credentialConfig = CredentialConfig(basicToken = Some("test_token"))
+    val authenticationConfig = Some(
+      AuthenticationConfig(
+        authenticationType = "BasicAuthentication",
+        credentials = credentialConfig
+      )
+    )
+
+    val restonomerRequest = RestonomerRequest(requestConfig)
+    val httpRequest = restonomerRequest.httpRequest
+
+    restonomerRequest.authenticate(authenticationConfig) shouldBe a[RestonomerRequest]
+    restonomerRequest.authenticate(authenticationConfig).httpRequest shouldNot be theSameInstanceAs httpRequest
+  }
+
+}
