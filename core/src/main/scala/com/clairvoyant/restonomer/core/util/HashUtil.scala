d8f3aab701b829188f4035e504e776f7d320409d
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-08-09 17:10:01 +0530

    Implemented SHA-256 Hash Algorithm

--- core/src/main/scala/com/clairvoyant/restonomer/core/util/HashUtil.scala
+++ core/src/main/scala/com/clairvoyant/restonomer/core/util/HashUtil.scala
@@ -9,16 +9,29 @@ object HashUtil {
   val DEFAULT_CHARSET_NAME = "UTF-8"
 
   val MD5_ALGORITHM_NAME = "MD5"
+  val SHA256_ALGORITHM_NAME = "SHA-256"
+
+  def getEncryptedHash(
+      text: String,
+      algorithmName: String,
+      format: String,
+      charsetName: String
+  ): String =
+    String.format(
+      format,
+      new BigInteger(MessageDigest.getInstance(algorithmName).digest(text.getBytes(charsetName)))
+    )
 
   def getMD5EncryptedHash(
       text: String,
       format: String = DEFAULT_STRING_FORMAT,
       charsetName: String = DEFAULT_CHARSET_NAME
-  ): String = {
-    String.format(
-      format,
-      new BigInteger(MessageDigest.getInstance(MD5_ALGORITHM_NAME).digest(text.getBytes(charsetName)))
-    )
-  }
+  ): String = getEncryptedHash(text, MD5_ALGORITHM_NAME, format, charsetName)
+
+  def getSHA256EncryptedHash(
+      text: String,
+      format: String = DEFAULT_STRING_FORMAT,
+      charsetName: String = DEFAULT_CHARSET_NAME
+  ): String = getEncryptedHash(text, SHA256_ALGORITHM_NAME, format, charsetName)
 
 }
