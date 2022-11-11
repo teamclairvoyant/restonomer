8ced2864aeef820908be769de00b38343d072d5e
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-11-11 14:26:48 -0500

    Added unit test for DataFrameToFileSystemWriter spec

--- core/src/test/scala/com/clairvoyant/restonomer/core/CoreSpec.scala
+++ core/src/test/scala/com/clairvoyant/restonomer/core/CoreSpec.scala
@@ -1,12 +1,17 @@
 package com.clairvoyant.restonomer.core
 
+import com.clairvoyant.restonomer.spark.utils.DataFrameMatchers
 import org.apache.spark.sql.SparkSession
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import sttp.client3._
 import sttp.model.Method
 
-trait CoreSpec extends AnyFlatSpec with Matchers {
+import java.nio.file.Files
+
+trait CoreSpec extends AnyFlatSpec with Matchers with BeforeAndAfterAll with DataFrameMatchers {
+
   val resourcesPath = "core/src/test/resources"
   val url = "/test_url"
   val uri = s"http://localhost:8080$url"
@@ -20,5 +25,8 @@ trait CoreSpec extends AnyFlatSpec with Matchers {
     .builder()
     .master("local[*]")
     .getOrCreate()
+
+  lazy val dataFrameToFileSystemWriterOutputDirPath: String =
+    Files.createTempDirectory(s"out_${System.currentTimeMillis()}").toString
 
 }
--- core/src/test/scala/com/clairvoyant/restonomer/core/persistence/FileSystemPersistenceSpec.scala
+++ core/src/test/scala/com/clairvoyant/restonomer/core/persistence/FileSystemPersistenceSpec.scala
@@ -1,22 +1,42 @@
 package com.clairvoyant.restonomer.core.persistence
 
 import com.clairvoyant.restonomer.core.CoreSpec
+import com.clairvoyant.restonomer.spark.utils.writer.DataFrameToFileSystemWriter
+import org.apache.commons.io.FileUtils
+import org.apache.spark.sql.DataFrame
+
+import java.io.File
 
 class FileSystemPersistenceSpec extends CoreSpec {
 
   import sparkSession.implicits._
 
+  val restonomerResponseDF: DataFrame = Seq(("val_A", "val_B", "val_C", "val_D"))
+    .toDF("col_A", "col_B", "col_C", "col_D")
+
   "persist() - with proper format and path" should
-    "save the dataframe to the file at the desired path in desired format" in {
+    "save the dataframe to the file in the desired format at the desired path" in {
       val fileSystemPersistence = FileSystem(
         fileFormat = "json",
-        filePath = ""
+        filePath = dataFrameToFileSystemWriterOutputDirPath
       )
 
-      val responseDF = Seq(("val_A", "val_B", "val_C", "val_D"))
-        .toDF("col_A", "col_B", "col_C", "col_D")
+      fileSystemPersistence.persist(
+        restonomerResponseDF,
+        new DataFrameToFileSystemWriter(
+          sparkSession = sparkSession,
+          fileFormat = fileSystemPersistence.fileFormat,
+          filePath = fileSystemPersistence.filePath
+        )
+      )
 
-      fileSystemPersistence.persist(responseDF)
+      sparkSession.read.json(dataFrameToFileSystemWriterOutputDirPath) should matchExpectedDataFrame(
+        restonomerResponseDF
+      )
     }
+
+  override def afterAll(): Unit = {
+    FileUtils.deleteDirectory(new File(dataFrameToFileSystemWriterOutputDirPath))
+  }
 
 }
