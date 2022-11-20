df1cba4c1b5d0f2d9077952607a85e46a8c00acc
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-11-20 13:37:26 -0500

    Added tests for token response placeholders

--- core/src/it/scala/com/clairvoyant/restonomer/token/TokenIntegrationTest.scala
+++ core/src/it/scala/com/clairvoyant/restonomer/token/TokenIntegrationTest.scala
@@ -6,11 +6,20 @@ class TokenIntegrationTest extends IntegrationTestDependencies {
 
   override val mappingsDirectory: String = "token"
 
-  it should "trigger the token request and get the required credential" in {
-    runCheckpoint(checkpointFileName = "checkpoint_token_request.conf")
+  it should "trigger the token request and get the required credential from the token response body" in {
+    runCheckpoint(checkpointFileName = "checkpoint_token_response_body.conf")
 
-    val outputDF = readOutputJSON()
-    val expectedDF = readExpectedMockJSON(fileName = "expected_token_request.json")
+    val outputDF = readOutputJSON(outputDirectoryName = "response_body")
+    val expectedDF = readExpectedMockJSON(fileName = "expected_test_token_response_body.json")
+
+    outputDF should matchExpectedDataFrame(expectedDF)
+  }
+
+  it should "trigger the token request and get the required credential from the token response headers" in {
+    runCheckpoint(checkpointFileName = "checkpoint_token_response_headers.conf")
+
+    val outputDF = readOutputJSON(outputDirectoryName = "response_headers")
+    val expectedDF = readExpectedMockJSON(fileName = "expected_test_token_response_headers.json")
 
     outputDF should matchExpectedDataFrame(expectedDF)
   }
