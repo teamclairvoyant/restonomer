1f7a9256384f48049bb2729d91f3cd6ddc535acd
kumar umang <umang.kumar@clairvoyantsoft.com>
2022-11-24 16:20:22 +0530

    added test cases for jwt authentication

---
+++ core/src/it/scala/com/clairvoyant/restonomer/authentication/JwtAuthenticationIntegrationTest.scala
@@ -0,0 +1,14 @@
+package com.clairvoyant.restonomer.authentication
+
+import com.clairvoyant.restonomer.common.IntegrationTestDependencies
+
+class JwtAuthenticationIntegrationTest extends IntegrationTestDependencies {
+  override val mappingsDirectory: String = "authentication/jwt_authentication"
+
+  it should "authenticate request with subject and secret-key" in {
+    runCheckpoint(checkpointFileName =
+      s"checkpoint_jwt_authentication.conf"
+    )
+  }
+
+}
