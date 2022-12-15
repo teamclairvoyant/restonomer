9293116917c41fce2363695806fbc0b281150c6d
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-12-15 17:28:02 -0500

    Implementation + Testing

---
+++ core/src/it/scala/com/clairvoyant/restonomer/query_params/QueryParamsIntegrationTest.scala
@@ -0,0 +1,18 @@
+package com.clairvoyant.restonomer.query_params
+
+import com.clairvoyant.restonomer.common.IntegrationTestDependencies
+
+class QueryParamsIntegrationTest extends IntegrationTestDependencies {
+
+  override val mappingsDirectory: String = "query_params"
+
+  it should "add query params to the request url" in {
+    runCheckpoint(checkpointFileName = "checkpoint_add_query_params.conf")
+
+    val outputDF = readOutputJSON()
+    val expectedDF = readExpectedMockJSON("expected_query_params.json")
+
+    outputDF should matchExpectedDataFrame(expectedDF)
+  }
+
+}
