702a41729cbb8075fbd0ed816fdd864ceb8335bb
Mamta Gupta <mamta.Gupta@clairvoyantsoft.com>
2023-01-08 21:23:21 +0530

    Replace String Value

---
+++ restonomer-core/src/test/scala/com/clairvoyant/restonomer/core/transformation/ReplaceStringInColumnValueTransformationSpec.scala
@@ -0,0 +1,48 @@
+package com.clairvoyant.restonomer.core.transformation
+
+import com.clairvoyant.restonomer.core.common.CoreSpec
+import com.clairvoyant.restonomer.spark.utils.DataFrameMatchers
+import org.apache.spark.sql.DataFrame
+
+class ReplaceStringInColumnValueTransformationSpec extends CoreSpec with DataFrameMatchers {
+  import sparkSession.implicits._
+
+  val restonomerResponseDF: DataFrame= Seq(("val_A", "val_B", "val_E")).toDF("col_A", "col_B", "col_C")
+
+  "transform() - with column-Replace-value" should "transform the dataframe as expected" in {
+    val restonomerTransformation = ReplaceStringInColumnValue(
+      columnName = "col_D",
+      columnValue = "val_D",
+      columnReplaceValue = "value_D"
+    )
+
+    val expectedRestonomerResponseTransformedDF = Seq(("val_A", "val_B", "val_C", "val_D"))
+      .toDF("col_A", "col_B", "col_C", "col_D")
+
+    val actualRestonomerResponseTransformedDF = restonomerTransformation.transform(restonomerResponseDF)
+
+    actualRestonomerResponseTransformedDF should matchExpectedDataFrame(
+      expectedDF = expectedRestonomerResponseTransformedDF
+    )
+  }
+
+  "transform() - without column-replace-value" should "transform the dataframe as expected" in {
+    val restonomerTransformation = ReplaceStringInColumnValue(
+      columnName = "col_D",
+      columnValue = "val_D",
+      columnReplaceValue = ""
+    )
+
+    val expectedRestonomerResponseTransformedDF = Seq(("val_A", "val_B", "val_C", "val_D"))
+      .toDF("col_A", "col_B", "col_C", "col_D")
+
+    val actualRestonomerResponseTransformedDF = restonomerTransformation.transform(restonomerResponseDF)
+
+    actualRestonomerResponseTransformedDF should matchExpectedDataFrame(
+      expectedDF = expectedRestonomerResponseTransformedDF
+    )
+  }
+
+
+
+}
