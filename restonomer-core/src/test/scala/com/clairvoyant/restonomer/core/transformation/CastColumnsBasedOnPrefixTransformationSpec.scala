ee732cd153d877c12083728ed3c602dfecb50785
Mamta Gupta <mamta.Gupta@clairvoyantsoft.com>
2023-01-28 20:49:27 +0530

    CastColumns-BasedOnPrefix

---
+++ restonomer-core/src/test/scala/com/clairvoyant/restonomer/core/transformation/CastColumnsBasedOnPrefixTransformationSpec.scala
@@ -0,0 +1,28 @@
+package com.clairvoyant.restonomer.core.transformation
+
+import com.clairvoyant.restonomer.core.common.CoreSpec
+import com.clairvoyant.restonomer.spark.utils.DataFrameMatchers
+import org.apache.spark.sql.DataFrame
+
+class CastColumnsBasedOnPrefixTransformationSpec extends CoreSpec with DataFrameMatchers{
+  import sparkSession.implicits._
+
+  val restonomerResponseDF: DataFrame = Seq(("val_A", "1", "4")).toDF("col_A", "col_B", "col_C")
+
+  "transform() - with column-data-type" should "transform the dataframe as expected" in {
+    val restonomerTransformation = CastColumnsBasedOnPrefix(
+      columnNames = List("col_B", "col_C"),
+      dataTypeToCast = "String"
+    )
+
+    val expectedRestonomerResponseTransformedDF = Seq(("val_A", "1", "4"))
+      .toDF("col_A", "col_B", "col_C")
+
+    val actualRestonomerResponseTransformedDF = restonomerTransformation.transform(restonomerResponseDF)
+
+    actualRestonomerResponseTransformedDF should matchExpectedDataFrame(
+      expectedDF = expectedRestonomerResponseTransformedDF
+    )
+  }
+
+}
