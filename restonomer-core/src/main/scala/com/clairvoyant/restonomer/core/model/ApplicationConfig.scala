b09d11d667fcdf24cb8343ddccdd87ced99a23b7
rahulbhatia023 <rahul.bhatia023@gmail.com>
2023-03-03 16:06:50 -0500

    Migration from scala2 to scala3

--- restonomer-core/src/main/scala/com/clairvoyant/restonomer/core/model/ApplicationConfig.scala
+++ restonomer-core/src/main/scala/com/clairvoyant/restonomer/core/model/ApplicationConfig.scala
@@ -1,11 +1,11 @@
 package com.clairvoyant.restonomer.core.model
 
 import zio.Config
-import zio.config._
-import zio.config.magnolia._
+import zio.config.*
+import zio.config.magnolia.*
 
 case class ApplicationConfig(sparkConfigs: Option[Map[String, String]])
 
 object ApplicationConfig {
-  given config: Config[ApplicationConfig] = deriveConfig[ApplicationConfig].mapKey(toKebabCase)
+  implicit val config: Config[ApplicationConfig] = deriveConfig[ApplicationConfig].mapKey(toKebabCase)
 }
--- restonomer-core/src/main/scala/com/clairvoyant/restonomer/core/model/CheckpointConfig.scala
+++ restonomer-core/src/main/scala/com/clairvoyant/restonomer/core/model/CheckpointConfig.scala
@@ -4,12 +4,12 @@ import zio.Config
 import zio.config.*
 import zio.config.magnolia.*
 
-object CheckpointConfig {
-  given config: Config[CheckpointConfig] = deriveConfig[CheckpointConfig].mapKey(toKebabCase)
-}
-
 case class CheckpointConfig(
     name: String,
     token: Option[TokenConfig],
     data: DataConfig
 )
+
+object CheckpointConfig {
+  implicit val config: Config[CheckpointConfig] = deriveConfig[CheckpointConfig].mapKey(toKebabCase)
+}
