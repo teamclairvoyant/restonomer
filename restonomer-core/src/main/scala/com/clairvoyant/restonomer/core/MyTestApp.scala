cc659d85be389a6566686869772589eabcbc72a1
rahulbhatia023 <rahul.bhatia023@gmail.com>
2023-03-08 14:13:35 -0500

    Migration from scala2 to scala3

---
+++ restonomer-core/src/main/scala/com/clairvoyant/restonomer/core/MyTestApp.scala
@@ -0,0 +1,12 @@
+package com.clairvoyant.restonomer.core
+
+import com.clairvoyant.restonomer.core.app.RestonomerContext
+
+object MyTestApp extends App {
+
+  private val restonomerContextDirectoryPath = "./my_restonomer_context"
+  private val restonomerContext = RestonomerContext(restonomerContextDirectoryPath)
+
+  restonomerContext.runAllCheckpoints()
+
+}
