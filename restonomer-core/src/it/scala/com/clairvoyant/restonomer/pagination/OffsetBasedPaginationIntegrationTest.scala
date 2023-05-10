a6642552a26128f98183ced8d80b3f49ba693da6
rahulbhatia023 <rahul.bhatia023@gmail.com>
2023-05-10 14:02:17 -0400

    Added integration test

---
+++ restonomer-core/src/it/scala/com/clairvoyant/restonomer/pagination/OffsetBasedPaginationIntegrationTest.scala
@@ -0,0 +1,15 @@
+package com.clairvoyant.restonomer.pagination
+
+import com.clairvoyant.restonomer.common.IntegrationTestDependencies
+import com.clairvoyant.restonomer.common.MockFileSystemPersistence
+
+class OffsetBasedPaginationIntegrationTest extends IntegrationTestDependencies with MockFileSystemPersistence {
+
+  override val mappingsDirectory: String = "pagination"
+
+  it should "perform pagination and get the grouped responses" in {
+    runCheckpoint(checkpointFileName = "checkpoint_offset_based_pagination.conf")
+    outputDF should matchExpectedDataFrame("expected_offset_based_pagination.json")
+  }
+
+}
