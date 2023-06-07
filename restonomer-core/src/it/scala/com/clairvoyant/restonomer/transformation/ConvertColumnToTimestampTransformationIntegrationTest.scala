21df80114c3366d70c94c5e0691569c0537381c0
rahulbhatia023 <rahul.bhatia023@gmail.com>
2023-06-07 16:46:38 -0400

    Adding integration test for ConvertColumnToTimestamp transformation

--- restonomer-core/src/it/scala/com/clairvoyant/restonomer/transformation/ConvertColumnToTimestampTransformationIntegrationTest.scala
+++ restonomer-core/src/it/scala/com/clairvoyant/restonomer/transformation/ConvertColumnToTimestampTransformationIntegrationTest.scala
@@ -1,14 +1,17 @@
 package com.clairvoyant.restonomer.transformation
 
 import com.clairvoyant.restonomer.common.{IntegrationTestDependencies, MockFileSystemPersistence}
+import org.apache.spark.sql.types.{StructField, StructType, TimestampType}
 
-class ConvertColumnToTimestampTransformationIntegrationTest extends IntegrationTestDependencies with MockFileSystemPersistence {
+class ConvertColumnToTimestampTransformationIntegrationTest
+    extends IntegrationTestDependencies
+    with MockFileSystemPersistence {
 
   override val mappingsDirectory: String = "transformation"
 
   it should "convert the specified column to Timestamp type" in {
     runCheckpoint(checkpointFileName = "checkpoint_convert_column_to_timestamp_transformation.conf")
-    outputDF should matchExpectedDataFrame("expected_split_column_transformation.json")
+    outputDF should matchExpectedDataFrame("expected_convert_column_to_timestamp_transformation.json")
   }
 
 }
